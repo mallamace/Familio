1fdbc5299b2c2489fc9219834620bd729a8a7fd5
mallamace <nathan@cruzeo.com>
2018-05-15 23:10:02 -0700

    V2 Updates

--- app/src/main/java/com/mallamace/famlio/TrackActivity.java
+++ app/src/main/java/com/mallamace/famlio/TrackActivity.java
@@ -2,8 +2,14 @@ package com.mallamace.famlio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TrackActivity extends AppCompatActivity {
+
+
+    boolean playing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,33 @@ public class TrackActivity extends AppCompatActivity {
         setContentView(R.layout.track_detail);
 
 
+        String TrackNameVal = getIntent().getExtras().getString("trackTitle");
+
+        TextView trackName = findViewById(R.id.trackTitle);
+        trackName.setText(TrackNameVal);
+
+
+        String TrackFileVal = getIntent().getExtras().getString("trackFile");
+
+        TextView trackFile = findViewById(R.id.trackFile);
+        trackFile.setText(TrackFileVal);
+
+
     }
+
+
+    void togglePlay(View v){
+
+        ImageView playButton = findViewById(R.id.playButtonId);
+
+
+        if(playing){ playing = false; playButton.setImageResource(R.drawable.pausebutton);} else {playing = true; playButton.setImageResource(R.drawable.playbutton);}
+
+
+
+
+    }
+
+
 
 }
--- app/src/main/java/com/mallamace/famlio/TrackAdapter.java
+++ app/src/main/java/com/mallamace/famlio/TrackAdapter.java
@@ -1,6 +1,7 @@
 package com.mallamace.famlio;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.content.ContextCompat;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,8 +17,15 @@ import java.util.ArrayList;
 public class TrackAdapter extends ArrayAdapter<Track> {
 
 
+    private Context context;
+    private String currentTrackTitle = "";
+    private String currentTrackFile = "";
+
+
     public TrackAdapter(Activity context, ArrayList<Track> trackFlavors) {
         super(context, 0, trackFlavors);
+
+        this.context = context;
     }
 
 
@@ -30,12 +39,15 @@ public class TrackAdapter extends ArrayAdapter<Track> {
         }
 
         if (position % 2 == 1) {
-            listItemView.setBackgroundColor(getContext().getTheme().getResources().getColor(R.color.listColorFirst));
+            listItemView.setBackgroundColor(context.getTheme().getResources().getColor(R.color.listColorFirst));
         } else {
-            listItemView.setBackgroundColor(getContext().getTheme().getResources().getColor(R.color.listColorSecond));
+            listItemView.setBackgroundColor(context.getTheme().getResources().getColor(R.color.listColorSecond));
         }
 
         Track currentTrackFlavor = getItem(position);
+
+        currentTrackTitle = currentTrackFlavor.getTrackTitleName();
+        currentTrackFile = currentTrackFlavor.getTrackFileName();
 
         TextView nameTextView = (TextView) listItemView.findViewById(R.id.audioTitle);
         nameTextView.setText(currentTrackFlavor.getTrackTitleName());
@@ -44,15 +56,18 @@ public class TrackAdapter extends ArrayAdapter<Track> {
         numberTextView.setText(currentTrackFlavor.getTrackFileName());
 
 
-//        TextView categoryItem = (TextView) findViewById(R.id.categoryItem);
-//        categoryItem.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent categoryIntent = new Intent(MainActivity.this, TrackActivity.class);
-//                categoryIntent.putExtra("catKeyName",2);
-//                startActivity(categoryIntent);
-//            }
-//        });
+
+
+        LinearLayout categoryItem = (LinearLayout) listItemView.findViewById(R.id.categoryItem);
+        categoryItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent categoryIntent = new Intent(context, TrackActivity.class);
+                categoryIntent.putExtra("trackTitle",currentTrackTitle);
+                categoryIntent.putExtra("trackFile",currentTrackFile);
+                getContext().startActivity(categoryIntent);
+            }
+        });
 
         return listItemView;
     }
